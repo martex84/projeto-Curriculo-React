30becf9d491db6653ad79a82bb77fc37d4f7beea
martex84 <martex849@gmail.com>
2022-01-04 09:04:34 -0300

    feat: Inclusion interface of Personal Information

--- frontend/src/view/components/menuSystem/index.jsx
+++ frontend/src/view/components/menuSystem/index.jsx
@@ -2,47 +2,47 @@ import "../../styles/menuSystem.css"
 
 function menuSystem() {
     return (
-        <section id="c-menuSystem">
-            <div className="c-secundario-menu displayMain">
-                <div className="c-informacao-menu displayMain">
-                    <button>Enviar Imagem de Perfil</button>
+        <section id="c-menu-system">
+            <div className="c-secundario-menu">
+                <div className="c-information-menu">
+                    <button className="button-system button-system--big">Enviar Imagem de Perfil</button>
                 </div>
-                <div className="c-informacao-menu displayMain">
-                    <h2>
+                <div className="c-information-menu">
+                    <h2 className="c-information-menu__title">
                         Informação Pessoal
                     </h2>
-                    <label>
+                    <label className="c-information-menu__label">
                         Nome
                     </label>
-                    <input type="text" placeholder="Digite seu nome" />
-                    <label>
+                    <input type="text" placeholder="Digite seu nome" className="c-information-menu__input" />
+                    <label className="c-information-menu__label">
                         Cidade
                     </label>
-                    <input type="text" placeholder="Digite sua cidade" />
-                    <label>
+                    <input type="text" placeholder="Digite sua cidade" className="c-information-menu__input" />
+                    <label className="c-information-menu__label">
                         Estado
                     </label>
-                    <input type="text" placeholder="Digite seu estado" />
-                    <label>
+                    <input type="text" placeholder="Digite seu estado" className="c-information-menu__input" />
+                    <label className="c-information-menu__label">
                         Telefone
                     </label>
-                    <input type="text" placeholder="Digite seu telefone" />
-                    <label>
+                    <input type="text" placeholder="Digite seu telefone" className="c-information-menu__input" />
+                    <label className="c-information-menu__label">
                         Email
                     </label>
-                    <input type="email" placeholder="Digite seu email" />
-                    <label>
+                    <input type="email" placeholder="Digite seu email" className="c-information-menu__input" />
+                    <label className="c-information-menu__label">
                         Github
                     </label>
-                    <input type="text" placeholder="Digite seu github" />
-                    <label>
+                    <input type="text" placeholder="Digite seu github" className="c-information-menu__input" />
+                    <label className="c-information-menu__label">
                         Linkedin
                     </label>
-                    <input type="text" placeholder="Digite seu linkedin" />
+                    <input type="text" placeholder="Digite seu linkedin" className="c-information-menu__input" />
                 </div>
             </div>
             <div className="c-secundario-menu">
-                <div className="c-informacao-menu">
+                <div className="c-information-menu">
 
                 </div>
             </div>
