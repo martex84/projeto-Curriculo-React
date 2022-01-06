a558cf7eeb6eef5b16b9969a1f114d5ce96b6e9b
martex84 <martex849@gmail.com>
2022-01-06 17:03:23 -0300

    feat: Inicialization for correction in input for Personal Information

--- frontend/src/view/components/MenuSystem/index.jsx
+++ frontend/src/view/components/MenuSystem/index.jsx
@@ -4,11 +4,53 @@ import "../../styles/menuSystem.css"
 
 import ListValues from "../ListValues";
 import * as processDatabase from '../../../services/processDatabase'
+import * as localStorageSystem from '../../../services/localStorage'
 
 function MenuSystem() {
 
+    const [objectInternalValues, setObjetoInternalValues] = useState({
+        personalInformation: {
+            name: "",
+            job: "",
+            city: "",
+            state: "",
+            phone: "",
+            email: "",
+            github: "",
+            linkedin: ""
+        },
+        profissionalExperience: {
+            companyName: "",
+            experience: ""
+        },
+        completeCourses: {
+            courseName: "",
+            institutionName: ""
+        },
+        languages: {
+            nameLanguages: "",
+            languageLevel: ""
+        },
+        abilities: [],
+        profissionalQualifications: []
+    })
     const [objetoReturnListValues, setObjetoReturnListValues] = useState();
     const [showListValues, setShowListValues] = useState(false);
+    const [localStorageVerified, setLocalStorageVerified] = useState([false, false]);
+    const [name, setName] = useState("");
+
+
+
+    if (localStorageSystem.checkLocalStorage() && localStorageVerified[0] === false) {
+        setLocalStorageVerified([true, false]);
+    }
+    //Check that localStorage can be create, if not, will show a message
+    else {
+
+        if (!localStorageSystem.createLocalStorage()) {
+            console.log("Error for create localStorage!");
+        }
+    }
 
     function openListValues(tipoObjeto) {
 
@@ -78,6 +120,71 @@ function MenuSystem() {
         setShowListValues(false);
     }
 
+    function saveObjetoReturnListValues(type, value) {
+        console.log(objectInternalValues);
+        let objectInternal = objectInternalValues;
+
+        switch (type) {
+            case "personalInformationName":
+                setObjetoInternalValues({ ...objectInternal, personalInformation: { name: value } })
+                break;
+
+            case "personalInformationJob":
+                objectInternal.personalInformation.job = value;
+                break;
+
+            case "personalInformationCity":
+                objectInternal.personalInformation.city = value;
+                break;
+
+            case "personalInformationState":
+                objectInternal.personalInformation.state = value;
+                break;
+
+            case "personalInformationPhone":
+                objectInternal.personalInformation.phone = value;
+                break;
+
+            case "personalInformationEmail":
+                objectInternal.personalInformation.email = value;
+                break;
+
+            case "personalInformationGithub":
+                objectInternal.personalInformation.github = value;
+                break;
+
+            case "personalInformationLinkedin":
+                objectInternal.personalInformation.linkedin = value;
+                break;
+        }
+        console.log(objectInternal, value);
+
+        setObjetoInternalValues(objectInternal);
+    }
+
+    function includeLocalStorage(type) {
+        switch (type) {
+            case "personalInformation":
+                localStorageSystem.includeField("personalInformation", objectInternalValues.personalInformation)
+
+                break;
+
+            case "profissionalExperience":
+                localStorageSystem.includeField("profissionalExperience", "Teste Experiencia")
+                break;
+        }
+    }
+
+    useEffect(() => {
+
+        if (localStorageVerified[1] === false && localStorageVerified[0] === true) {
+            setObjetoInternalValues(localStorageSystem.getLocalStorage());
+
+            setLocalStorageVerified([true, true]);
+        }
+    }, [localStorageVerified])
+
+    useEffect(() => { console.log(objectInternalValues.personalInformation.name) }, [objectInternalValues])
     return (
         <>
             <section id="c-menu-system">
@@ -102,35 +209,42 @@ function MenuSystem() {
                             <label className="c-information-menu__label">
                                 Nome
                             </label>
-                            <input type="text" placeholder="Digite seu nome" className="c-information-menu__input c-information-menu__input--personal-information" />
+                            <input type="text" placeholder="Digite seu nome" className="c-information-menu__input c-information-menu__input--personal-information" onChange={(event) => saveObjetoReturnListValues("personalInformationName", event.target.value)} value={objectInternalValues.personalInformation.name} />
+
                             <label className="c-information-menu__label">
                                 Cargo
                             </label>
-                            <input type="text" placeholder="Digite o cargo desejado" className="c-information-menu__input c-information-menu__input--personal-information" />
+                            <input type="text" placeholder="Digite o cargo desejado" className="c-information-menu__input c-information-menu__input--personal-information" onChange={(event) => saveObjetoReturnListValues("personalInformationJob", event.target.value)} value={objectInternalValues.personalInformation.job} />
+
                             <label className="c-information-menu__label">
                                 Cidade
                             </label>
-                            <input type="text" placeholder="Digite sua cidade" className="c-information-menu__input c-information-menu__input--personal-information" />
+                            <input type="text" placeholder="Digite sua cidade" className="c-information-menu__input c-information-menu__input--personal-information" onChange={(event) => saveObjetoReturnListValues("personalInformationCity", event.target.value)} value={objectInternalValues.personalInformation.city} />
+
                             <label className="c-information-menu__label">
                                 Estado
                             </label>
-                            <input type="text" placeholder="Digite seu estado" className="c-information-menu__input c-information-menu__input--personal-information" />
+                            <input type="text" placeholder="Digite seu estado" className="c-information-menu__input c-information-menu__input--personal-information" onChange={(event) => saveObjetoReturnListValues("personalInformationState", event.target.value)} value={objectInternalValues.personalInformation.state} />
+
                             <label className="c-information-menu__label">
                                 Telefone
                             </label>
-                            <input type="text" placeholder="Digite seu telefone" className="c-information-menu__input c-information-menu__input--personal-information" />
+                            <input type="text" placeholder="Digite seu telefone" className="c-information-menu__input c-information-menu__input--personal-information" onChange={(event) => saveObjetoReturnListValues("personalInformationPhone", event.target.value)} value={objectInternalValues.personalInformation.phone} />
+
                             <label className="c-information-menu__label">
                                 Email
                             </label>
-                            <input type="email" placeholder="Digite seu email" className="c-information-menu__input c-information-menu__input--personal-information" />
+                            <input type="email" placeholder="Digite seu email" className="c-information-menu__input c-information-menu__input--personal-information" onChange={(event) => saveObjetoReturnListValues("personalInformationEmail", event.target.value)} value={objectInternalValues.personalInformation.email} />
+
                             <label className="c-information-menu__label">
                                 Github
                             </label>
-                            <input type="text" placeholder="Digite seu github" className="c-information-menu__input c-information-menu__input--personal-information" />
+                            <input type="text" placeholder="Digite seu github" className="c-information-menu__input c-information-menu__input--personal-information" onChange={(event) => saveObjetoReturnListValues("personalInformationGithub", event.target.value)} value={objectInternalValues.personalInformation.github} />
+
                             <label className="c-information-menu__label">
                                 Linkedin
                             </label>
-                            <input type="text" placeholder="Digite seu linkedin" className="c-information-menu__input c-information-menu__input--personal-information" />
+                            <input type="text" placeholder="Digite seu linkedin" className="c-information-menu__input c-information-menu__input--personal-information" onChange={(event) => saveObjetoReturnListValues("personalInformationLinkedin", event.target.value)} value={objectInternalValues.personalInformation.linkedin} />
                         </div>
 
                         {/* Interface Profissional Experience */}
@@ -148,7 +262,7 @@ function MenuSystem() {
                             <textarea placeholder="Digite a experiência" className="c-information-menu__input c-information-menu__input--text-area" />
                             <div className="c-information-menu-button">
                                 <button aria-label="buttonOpenList" className="button-system" onClick={() => openListValues('experience')}>Visualizar lista</button>
-                                <button className="button-system">Adicionar</button>
+                                <button className="button-system" onClick={() => includeLocalStorage('personalInformation')}>Adicionar</button>
                             </div>
                         </div>
                     </div>
