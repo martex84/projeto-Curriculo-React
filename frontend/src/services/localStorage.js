a6fe2317823b34b7147a559c42a032092d7dcc6a
martex84 <martex849@gmail.com>
2022-01-06 17:02:34 -0300

    feat: Include function for Include, check and create LocalStorage

--- frontend/src/services/localStorage.js
+++ frontend/src/services/localStorage.js
@@ -1,38 +1,38 @@
 
-const nomeCurriculo = "SCurriculo";
+const nameLocalStorage = "SCurriculo";
+
+let internalLocalStorage = "";
 
 function createLocalStorage() {
     let resultProcess = false;
 
     const templateLocalStorage = {
-        informacaoPessoal: {
+        personalInformation: {
             name: "",
-            cargo: "",
-            cidade: "",
-            estado: "",
-            telefone: "",
+            job: "",
+            city: "",
+            state: "",
+            phone: "",
             email: "",
             github: "",
             linkedin: ""
         },
-        experienciasProfissionais: null,
-        cursosRealizados: null,
-        idiomas: null,
-        habilidades: null,
-        qualificacoesProfissionais: null
+        profissionalExperience: [],
+        completeCourses: [],
+        languages: [],
+        abilities: [],
+        profissionalQualifications: []
     };
 
-    if (!localStorage.getItem(nomeCurriculo)) {
-        localStorage.setItem(nomeCurriculo, JSON.stringify(templateLocalStorage));
+    if (!localStorage.getItem(nameLocalStorage)) {
+        localStorage.setItem(nameLocalStorage, JSON.stringify(templateLocalStorage));
 
-        if (localStorage.getItem(nomeCurriculo)) {
+        if (localStorage.getItem(nameLocalStorage)) {
             resultProcess = true;
 
             return resultProcess;
         }
         else {
-            resultProcess = false;
-
             return resultProcess;
         }
     }
@@ -42,7 +42,55 @@ function createLocalStorage() {
     return resultProcess;
 }
 
+function checkLocalStorage() {
+    if (!localStorage.getItem(nameLocalStorage)) {
+        debugger
+        return false
+    }
+
+
+
+    return true;
+}
+
+//Field === type of field will be chance
+//Value === object or array for to include in field
+function includeField(field, value) {
+
+    if (checkLocalStorage === false) {
+        return false
+    }
+
+    internalLocalStorage = JSON.parse(localStorage.getItem(nameLocalStorage));
+
+    Object.keys(internalLocalStorage).forEach(data => {
+
+        if (data === field) {
+            if (data === "personalInformation") {
+
+                internalLocalStorage.personalInformation = value
+            }
+            else {
+                let internalArray = [...internalLocalStorage[`${data}`]]
+
+                internalArray.push(value);
+
+                internalLocalStorage[`${data}`] = internalArray;
+
+            }
+            localStorage.setItem(nameLocalStorage, JSON.stringify(internalLocalStorage));
+        }
+    })
+}
+
+function getLocalStorage() {
+    return JSON.parse(localStorage.getItem(nameLocalStorage));
+}
+
 export {
-    nomeCurriculo,
-    createLocalStorage
+    nameLocalStorage,
+    createLocalStorage,
+    includeField,
+    checkLocalStorage,
+    getLocalStorage
 }
